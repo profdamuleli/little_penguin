fa9aa899cd665cf1f54f686efd3a1b45a108083f
profdamuleli <profdamuleli@gmail.com>
2018-10-08 12:26:36 +0200

    little_p

--- Ass05/.main.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/ldamulel/little_p/Ass05/main.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /root/ldamulel/little_p/Ass05/main.ko /root/ldamulel/little_p/Ass05/main.o /root/ldamulel/little_p/Ass05/main.mod.o ;  true
--- Ass05/.tmp_versions/main.mod
+++
@@ -1,3 +0,0 @@
-/root/ldamulel/little_p/Ass05/main.ko
-/root/ldamulel/little_p/Ass05/main.o
-
