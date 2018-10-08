aec6f92be4c3d401af258453c0e24153d13dc36b
Lutendo DAMULELI <ldamulel@e4r14p11.wethinkcode.co.za>
2018-10-08 11:37:16 +0200

    little_p

--- Ass04/main.c
+++ Ass04/main.c
@@ -4,48 +4,47 @@
 #include <linux/hid.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lutendo Damuleli");
+MODULE_AUTHOR("Tshilidzi Tshivhula <pboyfunclub@gmail.com>");
 MODULE_DESCRIPTION("usb module");
 
-static int driverprobe(struct usb_interface *intf, const struct usb_device_id *id)
+static int driverprobe(struct usb_interface *intf,
+		const struct usb_device_id *id)
 {
-    printk(KERN_INFO "In the probe function\n");
-    return 0:
+	printk(KERN_INFO "In the probe function\n");
+	return 0;
 }
 
 static void driverdisconnect(struct usb_interface *intf)
 {
-    printk(KERN_INFO "A USB device disconnected\n");
+	printk(KERN_INFO "A USB device disconnected\n");
 }
 
 static const struct usb_device_id supported_d_table[] = {
-    { USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
-            USB_INTERFACE_SUBCLASS_BOOT,
-            USB_INTERFACE_PROTOCOL_KEYBOARD) },
-    
-    {},
+	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
+			     USB_INTERFACE_SUBCLASS_BOOT,
+			     USB_INTERFACE_PROTOCOL_KEYBOARD) },
+	{ },
 };
 
 MODULE_DEVICE_TABLE(usb, supported_d_table);
 
-static struct usb_driver ldamulel = {
-    .name = "ldamulel",
-    .id_table = supported_d_table,
-    .probe = driverprobe,
-    .disconnect = driverdisconnect,
+static struct usb_driver ttshivhu = {
+	.name = "ttshivhu",
+	.id_table = supported_d_table,
+	.probe = driverprobe,
+	.disconnect = driverdisconnect,
 };
 
 static int __init entry_point(void)
 {
-    printk(KERN_INFO "Hello world!\n");
-    retun usb_register(&ldamulel);
+	printk(KERN_INFO "Hello world!\n");
+	return usb_register(&ttshivhu);
 }
 
 static void __exit exit_point(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
-    usb_deregister(&ldamulel);
+	printk(KERN_INFO "Cleaning up module.\n");
+	usb_deregister(&ttshivhu);
 }
-
 module_init(entry_point);
 module_exit(exit_point);
